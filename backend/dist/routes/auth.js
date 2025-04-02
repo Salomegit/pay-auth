75f51aee37e9c807bed41ec8e125cc1ac2314306
Salomegit <sallygithinji72@gmail.com>
2025-04-02 17:05:10 +0300

    exoress backend removed.used django backend

--- backend/dist/routes/auth.js
+++
@@ -1,5 +0,0 @@
-import { createUser } from "../controllers/auth/register.js";
-import express from "express";
-const router = express.Router();
-router.post("/register", createUser);
-export default router;
