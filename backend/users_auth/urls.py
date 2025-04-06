881939654ecbb000040c3130aa792b3392f81ceb
Salomegit <sallygithinji72@gmail.com>
2025-04-06 23:13:45 +0300

    feat:update login endpoint to use BuildAccessTokenCookies view and add requirements.txt for project dependencies

--- backend/users_auth/urls.py
+++ backend/users_auth/urls.py
@@ -1,10 +1,13 @@
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    
 )
+
+from .views import BuildAccessTokenCookies
 from django.urls import path
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('login/', BuildAccessTokenCookies.as_view(), name='token_obtain'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
